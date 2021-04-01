a15670b73fa81f04ecc5c33595efe2229e31c3ff
Lovepreet Singh <singhl22@student.douglascollge.ca>
2021-04-01 09:36:45 -0700

    solved a merge conflict

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 import FormContainer from './Components/FormContainer/FormContainer';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from './Components/HomePage/HomePage';
-<<<<<<< HEAD
-//import SignInForm from './Components/SignInForm/SignInForm';
-=======
->>>>>>> development
 
 function App() {
   return (
@@ -15,10 +11,6 @@ function App() {
         <Switch>
           <Route path="/" exact component={FormContainer}></Route>
           <Route path="/home_page" exact component={HomePage}></Route>
-<<<<<<< HEAD
-          {/* <Route path="/signup" exact component={SignInForm}></Route> */}
-=======
->>>>>>> development
         </Switch>
       </Router>
     </div>
