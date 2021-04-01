8a6eb71d12a98bce5532001dcce4cfc251270062
Lovepreet Singh <singhl22@student.douglascollge.ca>
2021-03-31 18:27:29 -0700

    Basic template for start of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import FormContainer from './Components/FormContainer/FormContainer';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import HomePage from './Components/HomePage/HomePage';
+import SignInForm from './Components/SignInForm/SignInForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={FormContainer}></Route>
+          <Route path="/home_page" exact component={HomePage}></Route>
+          <Route path="/signup" exact component={SignInForm}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
