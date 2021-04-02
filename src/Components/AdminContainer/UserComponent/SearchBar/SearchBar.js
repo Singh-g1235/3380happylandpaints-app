6778f69d72350ce36afb15b8bb893ea745859b47
Navneet Chahal <4114navneet@gmail.com>
2021-04-02 13:16:15 -0700

    Added code for user and order features

---
+++ src/Components/AdminContainer/UserComponent/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function SearchBar(props) {
+    const {keyword,update,search} = props;
+   
+  return (
+      <div className="form-inline">
+        <div className="form-group mx-sm-3 mb-2">
+    <input 
+
+     value={keyword}
+     placeholder={"Enter UserId"} onChange={update}
+     className="form-control"
+    /></div><div className="form-group mx-sm-3 mb-2">
+    <button className="btn btn-warning text-dark" onClick={search}>Search</button></div>
+    </div>
+  );
+}
+
+export default SearchBar
--- src/Services/AdminServices.js
+++ src/Services/AdminServices.js
@@ -1,3 +1,4 @@
+// Services for product begin
 export async function getProductsService() {
     // retrieve product list
     return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/products`)
@@ -47,3 +48,93 @@ export async function deleteProductService(productId) {
     })
     .then(response => response.json())
 }
+
+// Services for product ends
+
+
+
+
+//Services for User begin
+export async function getUsersService() {
+    // retrieve user list
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/users`)
+    .then(response => response.json())
+}
+
+
+export async function getUserByIdService(userId) {
+    //search user list by userId
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/user/${userId}`)
+    .then(response => {console.log(response); return response.json();})
+}
+
+
+export async function addUserService(user) {
+    //add user to database
+    let addedUser = user;
+    const newUser = JSON.stringify(addedUser);
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/user/`,{
+        method: 'POST',
+        headers: {"Content-Type":"application/json"},
+        body: newUser
+    })
+    .then(response => response.json())
+}
+
+export async function updateUserService(user) {
+    //update user in database
+
+    let updatedUser = user;
+    const newUser = JSON.stringify(updatedUser);
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/user/`,{
+        method: 'PUT',
+        headers: {"Content-Type":"application/json"},
+        body: newUser
+    })
+    .then(response => response.json())
+}
+
+export async function deleteUserService(userId) {
+    //remove user from database
+    const userToRemove = JSON.stringify({"UserId":userId});
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/user/`,{
+        method: 'DELETE',
+        headers: {"Content-Type":"application/json"},
+        body: userToRemove
+    })
+    .then(response => response.json())
+}
+
+//Services for user end
+
+
+
+//Services for Order begin
+export async function getOrdersService() {
+    // retrieve order list
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/orders`)
+    .then(response => response.json())
+}
+
+
+export async function getOrderByIdService(orderId) {
+    //search order list by orderId
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/order/${orderId}`)
+    .then(response => {console.log(response); return response.json();})
+}
+
+
+export async function updateOrderService(orderId) {
+    //update order in database
+    const newOrder = JSON.stringify({"OrderId":orderId});
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}admin/order/`,{
+        method: 'PUT',
+        headers: {"Content-Type":"application/json"},
+        body: newOrder
+    })
+    .then(response => response.json())
+}
+
+
+//Services for order end
+
